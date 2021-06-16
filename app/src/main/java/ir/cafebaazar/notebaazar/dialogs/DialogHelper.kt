00bfabf5437c52a0f8c1d129cefb6a7ddaa0af8c
sepehr <sepehrakhoundi1812@gmail.com>
2021-06-16 19:39:48 +0200

    dialog_confirm created.

---
+++ app/src/main/java/ir/cafebaazar/notebaazar/dialogs/DialogHelper.kt
@@ -0,0 +1,24 @@
+package ir.cafebaazar.notebaazar.dialogs
+
+import android.app.AlertDialog
+import android.widget.AdapterView
+import androidx.appcompat.app.AppCompatActivity
+import ir.cafebaazar.notebaazar.R
+
+object DialogHelper {
+
+    fun AppCompatActivity.showConfirmDialog(onConfirmSelectedListener: AdapterView.OnItemSelectedListener) {
+        AlertDialog.Builder(this)
+            .setView(
+                this.layoutInflater.inflate(
+                    R.layout.dialog_confirm,
+                    this.findViewById(R.id.coordinator_layout),
+                    false
+                )
+            )
+            .setOnItemSelectedListener(onConfirmSelectedListener)
+            .setCancelable(true)
+            .create()
+            .show()
+    }
+}
