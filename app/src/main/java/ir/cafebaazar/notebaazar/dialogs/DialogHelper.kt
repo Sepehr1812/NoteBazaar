8593ef84c42dfda4111b9392482c41ad6888b521
sepehr <sepehrakhoundi1812@gmail.com>
2021-06-16 20:47:59 +0200

    dialog_edit_folder created.

--- app/src/main/java/ir/cafebaazar/notebaazar/dialogs/DialogHelper.kt
+++ app/src/main/java/ir/cafebaazar/notebaazar/dialogs/DialogHelper.kt
@@ -1,18 +1,21 @@
 package ir.cafebaazar.notebaazar.dialogs
 
+import android.app.Activity
 import android.app.AlertDialog
 import android.widget.AdapterView
-import androidx.appcompat.app.AppCompatActivity
 import ir.cafebaazar.notebaazar.R
 
 object DialogHelper {
 
-    fun AppCompatActivity.showConfirmDialog(onConfirmSelectedListener: AdapterView.OnItemSelectedListener) {
-        AlertDialog.Builder(this)
+    fun showConfirmDialog(
+        activity: Activity,
+        onConfirmSelectedListener: AdapterView.OnItemSelectedListener
+    ) {
+        AlertDialog.Builder(activity)
             .setView(
-                this.layoutInflater.inflate(
+                activity.layoutInflater.inflate(
                     R.layout.dialog_confirm,
-                    this.findViewById(R.id.coordinator_layout),
+                    activity.findViewById(R.id.coordinator_layout),
                     false
                 )
             )
@@ -22,12 +25,33 @@ object DialogHelper {
             .show()
     }
 
-    fun AppCompatActivity.showCreateFolderDialog(onConfirmSelectedListener: AdapterView.OnItemSelectedListener) {
-        AlertDialog.Builder(this)
+    fun showCreateFolderDialog(
+        activity: Activity,
+        onConfirmSelectedListener: AdapterView.OnItemSelectedListener
+    ) {
+        AlertDialog.Builder(activity)
             .setView(
-                this.layoutInflater.inflate(
+                activity.layoutInflater.inflate(
                     R.layout.dialog_new_folder,
-                    this.findViewById(R.id.coordinator_layout),
+                    activity.findViewById(R.id.coordinator_layout),
+                    false
+                )
+            )
+            .setOnItemSelectedListener(onConfirmSelectedListener)
+            .setCancelable(true)
+            .create()
+            .show()
+    }
+
+    fun showEditFolderDialog(
+        activity: Activity,
+        onConfirmSelectedListener: AdapterView.OnItemSelectedListener
+    ) {
+        AlertDialog.Builder(activity)
+            .setView(
+                activity.layoutInflater.inflate(
+                    R.layout.dialog_edit_folder,
+                    activity.findViewById(R.id.coordinator_layout),
                     false
                 )
             )
