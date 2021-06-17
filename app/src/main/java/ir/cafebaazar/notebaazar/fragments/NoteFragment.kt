ed1fe93bee6b7b6089b6ad1c392137c2e3c95776
sepehr <sepehrakhoundi1812@gmail.com>
2021-06-17 18:32:38 +0430

    add viewBinding.

--- app/src/main/java/ir/cafebaazar/notebaazar/fragments/NoteFragment.kt
+++ app/src/main/java/ir/cafebaazar/notebaazar/fragments/NoteFragment.kt
@@ -5,13 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import ir.cafebaazar.notebaazar.R
+import ir.cafebaazar.notebaazar.databinding.FragmentNoteBinding
 
 class NoteFragment : Fragment() {
+
+    private var _binding: FragmentNoteBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_note, container, false)
+    ): View {
+        _binding = FragmentNoteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
--- app/src/main/java/ir/cafebaazar/notebaazar/fragments/NoteListFragment.kt
+++ app/src/main/java/ir/cafebaazar/notebaazar/fragments/NoteListFragment.kt
@@ -5,13 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import ir.cafebaazar.notebaazar.R
+import ir.cafebaazar.notebaazar.databinding.FragmentNoteListBinding
 
 class NoteListFragment : Fragment() {
+
+    private var _binding: FragmentNoteListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_note_list, container, false)
+    ): View {
+        _binding = FragmentNoteListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
