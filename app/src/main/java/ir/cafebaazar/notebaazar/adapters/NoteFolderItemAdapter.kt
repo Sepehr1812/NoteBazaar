55007f6cd8f2a0e71ba59c09e4c1b125476fdfbc
sepehr <sepehrakhoundi1812@gmail.com>
2021-06-17 15:38:37 +0430

    create NoteFolderItemAdapter.

---
+++ app/src/main/java/ir/cafebaazar/notebaazar/adapters/NoteFolderItemAdapter.kt
@@ -0,0 +1,24 @@
+package ir.cafebaazar.notebaazar.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class NoteFolderItemAdapter : RecyclerView.Adapter<NoteFolderItemAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
